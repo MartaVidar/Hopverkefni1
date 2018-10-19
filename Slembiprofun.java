d1712cc28bb6fc02f9403c553774e68acbf5bf32
MartaVidar <martavidar@gmail.com>
2018-10-19 00:09:32 +0000

    byrjun

---
+++ Slembiprofun.java
@@ -0,0 +1,20 @@
+public class SlembiProfun {
+
+    // Skilar N-staka fylki með slembiheiltölum á bilinu [a, b]
+    // a og b eru heiltölur með a <= b
+    public static int[] slembiFylki(int N, int a, int b) {
+        
+		// Ykkar kóði hér
+		
+    }
+
+    public static void main(String[] args) 
+    {
+
+        int[] rnd = slembiFylki(25, -10, 10);
+
+        for (int i=0; i<rnd.length; i++)
+            System.out.print(rnd[i] + " ");
+        System.out.println();
+    } 
+}
